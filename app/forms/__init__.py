96937c8ecafa2e12cea99c85fffb735747d7cce7
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-11-14 07:43:41 -0500

    Delete legacy backend.

--- app/forms/__init__.py
+++
@@ -1,3 +0,0 @@
-from .login_form import LoginForm
-from .signup_form import SignUpForm
-from .room_form import RoomForm
--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .db import db, environment, SCHEMA
-from .user import User
-from .room import Room
-from .room_users import room_users
-from .chat_message import ChatMessage
